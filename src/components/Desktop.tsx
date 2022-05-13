67122bf0ab883a753546be5063eb4bac6e057f13
Agustín Vega <agustinvegadev@gmail.com>
2022-05-13 19:56:54 -0300

    Better UX/UI

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -7,6 +7,16 @@ export const Desktop = () => {
 
     return (
         <main className="hidden lg:flex p-[15px] min-h-screen overflow-x-hidden bg-[#1b1b1b] flex-col gap-[15px]">
+
+            <div className="grid grid-cols-2 gap-4 h-[45px]">
+                <div className="flex-grow px-4 py-2 rounded-t-[8px] text-white border border-white">
+                    <p>Markdown</p>
+                </div>
+                <div className="flex-grow px-4 py-2 rounded-t-[8px] text-white border border-white">
+                    <p>Preview</p>
+                </div>
+            </div>
+
             <div className="grid grid-cols-2 gap-4">
                 <Markdown />
                 <Preview />
